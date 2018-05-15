1fc08fa76069a9b794453cc2fff338c2d3c10dd7
KevinCHWu <kevinwu0703@gmail.com>
2018-05-15 14:12:00 -0700

    Add task and began them

--- tweets/index.js
+++ tweets/index.js
@@ -11,18 +11,18 @@ const INPUTFILENAME = 'trumptweets.csv'
 
 const stream = fs.createWriteStream(path.join(__dirname, INPUTFILENAME), { flags: 'a' })
 
+const dateOfWeek = csv => {
+  
+}
+
+const timeOfDay = csv => {
+
+}
+
 const readCSV = async () => {
   const file = await fsReadFile(path.join(__dirname, INPUTFILENAME))
   csvParse(file)
-    .then(csvContent => csvContent.map(tweet => tweet.Date))
-    .then(dates => R.groupWith(R.equals, dates))
-    .then(datesArr => datesArr.map(uniqDate => ({
-      date: uniqDate[0],
-      count: uniqDate.length
-    })))
-    .then(dateArrayCount => dateArrayCount.sort(function (a, b) {
-      return a.count - b.count
-    }))
+    .then(csv => dateOfWeek(csv))
     .then(x => console.log(x))
 }
 
