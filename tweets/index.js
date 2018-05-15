a56e17ac3d7856eefbbc2dada0f3b8c721b64141
Justin Lee <justinlee0022@gmail.com>
2018-05-15 12:37:25 -0700

    Fixed up repo

--- tweets/index.js
+++ tweets/index.js
@@ -5,13 +5,14 @@ const {
   fsWriteFile,
   csvParse
 } = require('./util/promises')
+const R = require('ramda')
 
 const INPUTFILENAME = 'trumptweets.csv'
 
-const stream = fs.createWriteStream(path.join(__dirname, '/output/', INPUTFILENAME), { flags: 'a' })
+const stream = fs.createWriteStream(path.join(__dirname, INPUTFILENAME), { flags: 'a' })
 
 const readCSV = async () => {
-  const file = await fsReadFile(path.join(__dirname, '/input/', INPUTFILENAME))
+  const file = await fsReadFile(path.join(__dirname, INPUTFILENAME))
   csvParse(file)
     .then(csvContent => csvContent.map(tweet => tweet.Date))
     .then(dates => R.groupWith(R.equals, dates))
